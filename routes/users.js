92a3ba4a0a12b8d31b4e3ccf1b6409eb3fa0433f
Faizan Gaima <faizangaima@Faizans-MacBook-Pro.local>
2019-01-14 23:06:55 +0530

    added login route

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const Users = require('../models/User');
-
+const secretKey = require('../config/keys')
 
 //Registration Post
 router.post('/register', (req, res) => {
@@ -25,9 +25,41 @@ router.post('/register', (req, res) => {
             .then(user => res.json(user))
             .catch(err => console.log(err));
         });
-      })
+      });
+    }
+  });
+});
+
+//Login Route
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  Users.findOne({ email }).then(user => {
+    if (!user) {
+      res.status(404).json({ email: "Email already exist" })
+    } else {
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+            const payload = {
+              id: user.id,
+              name: user.name,
+              email: user.email
+            }
+            jwt.sign(payload, secretKey.jwtSecretKey, { expiresIn: 604800 }, (err, token) => {
+              if (err) throw err;
+              res.json({
+                success: true,
+                token: 'Bearer' + token
+              })
+            })
+          } else {
+            res.status(400).json({ password: "Incorrect Password" })
+          }
+        }).catch(err => console.log(err))
     }
   })
-});
+})
 
 module.exports = router;
